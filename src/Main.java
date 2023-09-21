045a383c0e6ea876b8388fa371e411436f870fc9
BT_2N2_31 <sample@bths.edu>
2023-09-21 13:37:57 -0400

    more commit

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,10 @@ public class Main {
         int people = scan.nextInt();
         System.out.print("What's the tip percentage? (0-100): ");
         int percentage = scan.nextInt();
+        while (number % 2 != 0) {
+            System.out.print("Enter a cost in dollar and cents: ");
+            number = scan.nextInt();
+        }
+
     }
 }
